7c50aad6c1ef3ab5c13370cba390f4cb586584f1
cdrv5 <151071533+cdrv5@users.noreply.github.com>
2024-05-09 11:50:09 +0530

    Update learning_algo.py

--- learning_algo.py
+++ learning_algo.py
@@ -1,11 +1,26 @@
-def minimax_pairwise(numbers):
-    if len(numbers) == 1:
-        return numbers[0]
+X_train = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+y_train = ['A', 'A', 'B', 'B', 'B']  
 
-    min_values = []
-    for i in range(0, len(numbers)-1, 2):
-        pair_min = min(numbers[i], numbers[i + 1])
-        min_values.append(pair_min)
-    return max(min_values)
-numbers = [3, 5, 1, 9, 2, 7]
-print("Max value after pairwise min:", minimax_pairwise(numbers))
+def euclidean_distance(x1, x2):
+    
+    return ((x1[0] - x2[0]) * 2 + (x1[1] - x2[1]) * 2) ** 0.5
+
+def knn_predict(X_train, y_train, x_test, k=3):
+    distances = []
+    for i, x_train in enumerate(X_train):
+        distance = euclidean_distance(x_train, x_test)
+        distances.append((distance, i))
+
+    distances.sort()  
+    k_nearest = distances[:k]  
+
+    nearest_labels = [y_train[i] for _, i in k_nearest]
+    prediction = max(set(nearest_labels), key=nearest_labels.count)  
+    return prediction
+
+
+X_test = [2.5, 3.5]  
+k = 3  
+
+prediction = knn_predict(X_train, y_train, X_test, k)
+print("Predicted class:", prediction)
